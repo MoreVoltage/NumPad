896de51ddc40084ac80c82503c3698edbe9f3e22
Lasha Efremidze <efremidzel@hotmail.com>
2016-01-06 12:43:10 -0800

    Added Setting Bundle

--- NumPad/AppDelegate.swift
+++ NumPad/AppDelegate.swift
@@ -14,14 +14,14 @@ import Crashlytics
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
-
-
+    
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         // Override point for customization after application launch.
         Fabric.with([Crashlytics()])
+        updateSettingsBundle()
         return true
     }
-
+    
     func applicationWillResignActive(application: UIApplication) {
         // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
         // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
@@ -43,7 +43,19 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func applicationWillTerminate(application: UIApplication) {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
     }
-
-
+    
 }
 
+// MARK: -
+private extension AppDelegate {
+    
+    func updateSettingsBundle() {
+        if let version = NSBundle.mainBundle().objectForInfoDictionaryKey("CFBundleShortVersionString") as? String {
+            NSUserDefaults.standardUserDefaults().setObject(version, forKey: "Version")
+        }
+        if let build = NSBundle.mainBundle().objectForInfoDictionaryKey("CFBundleVersion") as? String {
+            NSUserDefaults.standardUserDefaults().setObject(build, forKey: "Build")
+        }
+    }
+    
+}
