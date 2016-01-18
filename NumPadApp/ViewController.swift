8530a79263cce16460997ae303e07d352eadf5db
Lasha Efremidze <efremidzel@hotmail.com>
2016-01-17 17:17:20 -0800

    BACKUP

--- NumPadApp/ViewController.swift
+++ NumPadApp/ViewController.swift
@@ -19,6 +19,8 @@ class ViewController: UIViewController {
             
             tableView.estimatedRowHeight = 44
             tableView.rowHeight = UITableViewAutomaticDimension
+            
+            tableView.keyboardDismissMode = .Interactive
         }
     }
     
@@ -33,22 +35,50 @@ class ViewController: UIViewController {
 extension ViewController: UITableViewDataSource {
     
     func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 1
+        return 2
     }
     
     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
-        let cell = UITableViewCell(style: .Default, reuseIdentifier: nil)
-        
-        var text: String?
         switch indexPath.row {
-        case 0: text = "Go to Settings → General → Keyboard → Keyboards → Add New Keyboard"
+        case 0:
+            let cell = UITableViewCell(style: .Default, reuseIdentifier: nil)
+            cell.textLabel?.text = "Go to Settings → General → Keyboard → Keyboards → Add New Keyboard"
+            cell.textLabel?.numberOfLines = 0
+            return cell
+        case 1:
+            let cell = TextFieldCell(style: .Default, reuseIdentifier: nil)
+            cell.textField.placeholder = "Test me"
+            return cell
         default: break
         }
-        cell.textLabel?.text = text
         
-        cell.textLabel?.numberOfLines = 0
+        return UITableViewCell()
+    }
+    
+}
+
+// MARK: - TextFieldCell
+private class TextFieldCell: UITableViewCell {
+    
+    let textField = UITextField()
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        setup()
+    }
+    
+    override init(style: UITableViewCellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        setup()
+    }
+    
+    func setup() {
+        textField.translatesAutoresizingMaskIntoConstraints = false
+        contentView.addSubview(textField)
         
-        return cell
+        let views = ["textField": textField]
+        addConstraints(NSLayoutConstraint.constraintsWithVisualFormat("H:|-20-[textField]|", options: [], metrics: nil, views: views))
+        addConstraints(NSLayoutConstraint.constraintsWithVisualFormat("V:|[textField]|", options: [], metrics: nil, views: views))
     }
     
 }
